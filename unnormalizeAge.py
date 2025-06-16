51e388562f23c498897d33697ac5bf6fcc777afc
loris <l.pennucci888@gmail.com>
2025-06-16 13:46:58 +0200

    rimosso normalizzazione di Age

---
+++ unnormalizeAge.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+df= pd.read_csv('dataset/heart-attack-risk-prediction-dataset.csv')
+df=df.dropna()
+df= df.drop(columns=['Diet', 'BMI', 'Heart Attack Risk (Text)', 'Sedentary Hours Per Day', 'Exercise Hours Per Week'])
+df = df.rename(columns={'Heart Attack Risk (Binary)': 'Heart Attack Risk'})
+
+min_age = 19
+max_age = 82
+
+df['Age'] = df['Age'] * (max_age - min_age) + min_age
+
+df.to_csv('dataset/mainDatasetAgeFix.csv', index=False)
